d2451fa8df568a04f4445a8fe0fe7f9e88841a7d
robyn <65057146+robynsdev@users.noreply.github.com>
2020-11-09 12:02:26 +1100

    added index, show, edit, update, destroy to posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,10 @@ class PostsController < ApplicationController
     @post = Post.new
   end  
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
   def create
     @post = Post.new(post_params)
  
@@ -19,6 +23,23 @@ class PostsController < ApplicationController
     else
       render 'new'
     end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+   
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+   
+    redirect_to posts_path
   end
    
   private
