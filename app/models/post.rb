c77ddae1ebe6725d3b6a2f89a0b909c33487e5d6
robyn <65057146+robynsdev@users.noreply.github.com>
2020-11-09 11:00:14 +1100

    added new, create to posts_controller and form in views/posts/new

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+  def new
+  end
+
+  def create
+    @post = Post.new(post_params)
+   
+    @post.save
+    redirect_to @post
+  end
+   
+  private
+    def post_params
+      params.require(:post).permit(:title, :text)
+    end    
+
+end
