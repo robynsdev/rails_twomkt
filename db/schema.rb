104f38fecc5506c4bd3b8e7bd1f770d350a02042
robyn <65057146+robynsdev@users.noreply.github.com>
2020-11-11 15:15:20 +1100

    added refs to transactions table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_11_012953) do
+ActiveRecord::Schema.define(version: 2020_11_11_035139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,17 @@ ActiveRecord::Schema.define(version: 2020_11_11_012953) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "transactions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "seller_id", null: false
+    t.bigint "buyer_id", null: false
+    t.bigint "post_id", null: false
+    t.index ["buyer_id"], name: "index_transactions_on_buyer_id"
+    t.index ["post_id"], name: "index_transactions_on_post_id"
+    t.index ["seller_id"], name: "index_transactions_on_seller_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -73,4 +84,7 @@ ActiveRecord::Schema.define(version: 2020_11_11_012953) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "users"
+  add_foreign_key "transactions", "posts"
+  add_foreign_key "transactions", "users", column: "buyer_id"
+  add_foreign_key "transactions", "users", column: "seller_id"
 end
