24aefa94f059c0ae81455df6ebf1380b429f0d41
robyn <65057146+robynsdev@users.noreply.github.com>
2020-11-13 19:35:27 +1100

    removed code added to initializer

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -13,9 +13,9 @@ Stripe.api_key = Rails.application.credentials.dig(:public_key, :secret_key)
 # Stripe.api_key = Rails.configuration.stripe[:secret_key]
 
 
-if Rails.env.production?
-  :public_key => ENV['PUBLIC_KEY'],
-  :secret_key => ENV['SECRET_KEY']
-end
+# if Rails.env.production?
+#   :public_key => ENV['PUBLIC_KEY'],
+#   :secret_key => ENV['SECRET_KEY']
+# end
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
